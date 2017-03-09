c273a3905c2f5e3097a2865c6d3fb117c074c151
Pullock <pullocked@gmail.com>
2017-03-09 23:06:54 +0800

    添加NettyTest

--- DubboProvider/src/main/java/dubbo/provider/hello/service/impl/EventServiceImpl.java
+++ DubboProvider/src/main/java/dubbo/provider/hello/service/impl/EventServiceImpl.java
@@ -1,12 +1,12 @@
-package dubbo.provider.hello.service.impl;
+/*package dubbo.provider.hello.service.impl;
 
 import dubbo.common.hello.service.EventService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
-/**
+*//**
  * Created by cheng.xi on 2017-02-28 10:23.
- */
+ *//*
 public class EventServiceImpl implements EventService,ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void testEvent() {
@@ -20,4 +20,4 @@ public class EventServiceImpl implements EventService,ApplicationListener<Contex
             System.out.println(" eventService Bean初始化完成之后，开始调用事件通知。。。。");
         }
     }
-}
+}*/
--- DubboProvider/src/main/java/dubbo/provider/hello/service/impl/HelloServiceImpl.java
+++ DubboProvider/src/main/java/dubbo/provider/hello/service/impl/HelloServiceImpl.java
@@ -1,5 +1,7 @@
 package dubbo.provider.hello.service.impl;
 
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.rpc.Protocol;
 import dubbo.common.hello.service.HelloService;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -7,18 +9,20 @@ import org.springframework.context.event.ContextRefreshedEvent;
 /**
  * Created by cmcc on 15/4/12.
  */
-public class HelloServiceImpl implements HelloService,ApplicationListener<ContextRefreshedEvent> {
+public class HelloServiceImpl implements HelloService/*,ApplicationListener<ContextRefreshedEvent>*/ {
     @Override
     public void sayHello() {
         System.out.println("这里是Provider");
         System.out.println("HelloWorld Provider！");
+        int defaultPort = ExtensionLoader.getExtensionLoader(Protocol.class).getExtension("dubbo").getDefaultPort();
+        System.out.println("defalut port : " + defaultPort);
     }
 
-    @Override
+    /*@Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         System.out.println(event);
         if(ContextRefreshedEvent.class.getName().equals(event.getClass().getName())){
             System.out.println("Bean初始化完成之后，开始调用事件通知。。。。");
         }
-    }
+    }*/
 }
