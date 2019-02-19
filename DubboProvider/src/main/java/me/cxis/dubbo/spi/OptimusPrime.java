db1f1224f0b557d58b8070d081975a3c8b782111
dachengxi <dachengdaxi@gmail.com>
2019-02-19 19:06:14 +0800

    dubbo spi test

---
+++ DubboProvider/src/main/java/me/cxis/dubbo/spi/OptimusPrime.java
@@ -0,0 +1,9 @@
+package me.cxis.dubbo.spi;
+
+public class OptimusPrime implements Robot {
+
+    @Override
+    public void sayHello() {
+        System.out.println("Optimus Prime.");
+    }
+}
---
+++ DubboProvider/src/main/java/me/cxis/dubbo/spi/Robot.java
@@ -0,0 +1,9 @@
+package me.cxis.dubbo.spi;
+
+import com.alibaba.dubbo.common.extension.SPI;
+
+@SPI
+public interface Robot {
+
+    void sayHello();
+}
