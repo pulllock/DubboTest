662180eca4a789260a8f51a914e7a8a45aeb0686
dachengxi <dachengdaxi@gmail.com>
2019-03-06 14:59:31 +0800

    generic service

---
+++ DubboCommon/src/main/java/me/cxis/dubbo/query/UserQuery.java
@@ -0,0 +1,36 @@
+package me.cxis.dubbo.query;
+
+import java.io.Serializable;
+
+public class UserQuery implements Serializable {
+
+    private static final long serialVersionUID = -6913251782089881011L;
+
+    private Long id;
+
+    private String userName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserQuery{" +
+            "id=" + id +
+            ", userName='" + userName + '\'' +
+            '}';
+    }
+}
